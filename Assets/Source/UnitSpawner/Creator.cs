cfa1aa249bd277aa0c045222f8cd5d53983f4f1a
Evgeny Vizul <99418283+EVVIGAL@users.noreply.github.com>
2022-12-28 16:47:30 +0700

    Add unit button

---
+++ Assets/Source/UnitSpawner/Creator.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Creator : MonoBehaviour
+{
+    [SerializeField] private Money _money;
+
+    private Vector3 _tankColliderSize;
+    private Collider[] _colliders;
+    private Camera _camera;
+    private Unit _unit;
+    private int _groundLayer = 3;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                if (_unit != null && _money.Amount >= _unit.Cost)
+                {
+                    Vector3 position = GetPosition();
+
+                    if (CheckSpawnPoint(position))
+                        SpawnUnit(position);
+                }
+            }
+        }
+    }
+
+    public void SetUnit(Unit unit)
+    {
+        _unit = unit;
+        _tankColliderSize = _unit.GetComponent<Collider>().bounds.extents;
+    }
+
+    private void SpawnUnit(Vector3 position)
+    {
+        Instantiate(_unit, position, Quaternion.identity);
+        _money.SpendMoney(_unit.Cost);
+    }
+
+    private Vector3 GetPosition()
+    {
+        Vector3 position;
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+            position = hit.point;
+        else
+            position = Vector3.zero;
+
+        return position;
+    }
+
+    private bool CheckSpawnPoint(Vector3 position)
+    {
+        _colliders = Physics.OverlapBox(position, _tankColliderSize, Quaternion.identity, _groundLayer);
+
+        if (_colliders.Length > 0)
+            return false;
+        else
+            return true;
+    }
+}
---
+++ Assets/Source/UnitSpawner/UnitChoser.cs
@@ -0,0 +1,34 @@
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class UnitChoser : MonoBehaviour
+{
+    [SerializeField] private Creator _creator;
+    [SerializeField] private Unit _unit;
+
+    private Button _button;
+    private TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _text = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(SetUnit);
+        _text.text = $"{_unit.Cost}";
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveAllListeners();
+    }
+
+    private void SetUnit()
+    {
+        _creator.SetUnit(_unit);
+    }
+}
