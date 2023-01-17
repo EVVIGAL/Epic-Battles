f86d66a67dbeb78037dba1566f1026e7a56a0100
Evgeny Vizul <99418283+EVVIGAL@users.noreply.github.com>
2023-01-17 12:11:18 +0700

    Add info panel. Draft version.

--- Assets/Source/UnitSpawner/Unit.cs
+++ Assets/Source/UnitSpawner/Unit.cs
@@ -1,14 +1,19 @@
-using UnityEngine.Events;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    [SerializeField] private int _cost;
+    [SerializeField] private Sprite _sprite;
     [SerializeField] private string _name;
+    [SerializeField] private string _skill;
+    [SerializeField] private int _cost;
 
-    public int Cost => _cost;
+    public Sprite Sprite => _sprite;
 
     public string Name => _name;
+
+    public string Skill => _skill;
+
+    public int Cost => _cost;
 
     public void Remove()
     {
