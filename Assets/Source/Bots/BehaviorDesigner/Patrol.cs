92fbb5b9e1083446fc37e3209038e48379d8ccc3
FarkhutdinovMR <farkhutdinovmr@gmail.com>
2023-01-18 17:13:45 +0500

    Add boss

---
+++ Assets/Source/Bots/BehaviorDesigner/Patrol.cs
@@ -0,0 +1,36 @@
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+public class Patrol : Action
+{
+    [SerializeField] private float _stopingDistance = 1f;
+    [SerializeField] private Transform _path;
+
+    public MonoBehaviour MovementSource;
+    private IMovement _movement => (IMovement)MovementSource;
+
+    private int _currentPointIndex;
+    private Transform[] _points;
+
+    public override void OnAwake()
+    {
+        base.OnAwake();
+        _points = _path.GetComponentsInChildren<Transform>();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, _points[_currentPointIndex].position);
+        if (distance <= _stopingDistance)
+        {
+            _currentPointIndex++;
+            if (_currentPointIndex >= _points.Length)
+                _currentPointIndex = 0;
+        }
+
+        Vector3 direction = (_points[_currentPointIndex].position - transform.position).normalized;
+        _movement.Move(new Vector2(direction.x, direction.z));
+
+        return TaskStatus.Running;
+    }
+}
