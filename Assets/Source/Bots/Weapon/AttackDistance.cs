2bd606eecebc47a0df770365a925b1abc2cbc7ff
FarkhutdinovMR <farkhutdinovmr@gmail.com>
2023-01-24 16:42:52 +0500

    Redesign town map

--- Assets/Source/Bots/Weapon/AttackDistance.cs
+++ Assets/Source/Bots/Weapon/AttackDistance.cs
@@ -5,6 +5,14 @@ public class AttackDistance : MonoBehaviour
     [SerializeField] private float _defaultValue;
 
     private const float SeaLevel = 3f;
+    private const float RandomOffsetDistance = 3f;
 
-    public float Value => transform.position.y > SeaLevel ? _defaultValue * 2f : _defaultValue;
+    private float _randomOffset;
+
+    private void Awake()
+    {
+        _randomOffset = Random.Range(-RandomOffsetDistance, RandomOffsetDistance);
+    }
+
+    public float Value => transform.position.y > SeaLevel ? _defaultValue * 2f + _randomOffset : _defaultValue + _randomOffset;
 }
