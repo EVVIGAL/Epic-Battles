f24427d7cfcea9a05de41634b64044a92272ce27
Evgeny Vizul <99418283+EVVIGAL@users.noreply.github.com>
2023-03-15 13:39:24 +0700

    Fix audio bug. Raplace EventSystem by WebEventSystem

--- Assets/Samples/Web Utility/2.0.0/Playtesting Sample/PlaytestingCanvas.cs
+++ Assets/Samples/Web Utility/2.0.0/Playtesting Sample/PlaytestingCanvas.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Agava.WebUtility.Samples
 {
     public class PlaytestingCanvas : MonoBehaviour
     {
+        [SerializeField] private Slider _slider;
+
         private void OnEnable()
         {
             WebApplication.InBackgroundChangeEvent += OnInBackgroundChange;
@@ -16,6 +19,12 @@ namespace Agava.WebUtility.Samples
 
         private void OnInBackgroundChange(bool inBackground)
         {
+            if (_slider != null)
+            {
+                if (_slider.value == 0)
+                    return;
+            }
+
             AudioListener.pause = inBackground;
             AudioListener.volume = inBackground ? 0f : 1f;
         }
